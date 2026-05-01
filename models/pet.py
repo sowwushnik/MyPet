26caa0400a834c2af358707b853b56aff7b543d1
sowwushnik-alt <253653@astanait.edu.kz>
2026-05-01 20:22:43 +0500

    Fourth commit
    What's changed:
    - Now program will check weight and age.
    - Changed Visual Feedback

--- models/pet.py
+++ models/pet.py
@@ -3,12 +3,28 @@ import math
 class Pet:
     def __init__(self, name, age, weight):
         self.name = name
-        self.age = age
-        self.weight = weight
+
+        if age < 0:
+            print(f"Warning: Age for {name} cannot be negative. Setting to 0")
+            self.age = 0
+        else:
+            self.age = age
+
+        if weight <= 0:
+            print(f"Warning: Weight for {name} must be positive. Setting to 1kg default")
+            self.weight = 1.0
+        else:
+            self.weight = weight
+
         self.health_logs = []
 
     def __str__(self):
-        return f"[{self.__class__.__name__}] {self.name}: {self.weight}kg, {self.age} years old"
+        return (f"---------------------------\n"
+                f"NAME: {self.name}\n"
+                f"TYPE: {self.__class__.__name__}\n"
+                f"AGE: {self.age} years\n"
+                f"WEIGHT: {self.weight} kg\n"
+                f"---------------------------")
 
     def calculate_daily_calories(self):
         return 70 * math.pow(self.weight, 0.75)
